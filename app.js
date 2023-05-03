6e83de685334274b0648ca99e668cbdb7b1bb4bb
Mehdi <mehdidini7@gmail.com>
2023-05-03 20:23:50 +0430

    Update styles

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ hamburger.addEventListener('click',()=> {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
+document.addEventListener('scroll', ()=> {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250 ){
+        header.style.backgroundColor = 'rgba(31, 30, 30, 0.24)'
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+})
